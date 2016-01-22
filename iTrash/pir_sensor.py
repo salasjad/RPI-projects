b99ed9519259887234d7356259355ee06a29be6b
root <root@raspberrypi.(none)>
2016-01-22 20:40:04 +0000

    pir_sensor code

--- iTrash/pir_sensor.py
+++ iTrash/pir_sensor.py
@@ -10,9 +10,10 @@ previous_state = False
 current_state = False
 
 while True:
-        time.sleep(0.1)
-            previous_state = current_state
-                current_state = GPIO.input(sensor)
-                    if current_state != previous_state:
-                                new_state = "HIGH" if current_state else "LOW"
-                                        print("GPIO pin %s is %s" % (sensor, new_state))
+	time.sleep(0.1)
+	previous_state = current_state
+	current_state = GPIO.input(sensor)
+                    
+	if current_state != previous_state:
+		new_state = "HIGH" if current_state else "LOW"
+		print("GPIO pin %s is %s" % (sensor, new_state))
