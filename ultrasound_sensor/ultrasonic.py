47cd69bce80b525072994649b5203a87feb598e0
root <root@raspberrypi.(none)>
2016-01-22 18:36:18 +0000

    Added requests json post mechanism

--- ultrasound_sensor/ultrasonic.py
+++ ultrasound_sensor/ultrasonic.py
@@ -1,5 +1,5 @@
 import json
-import urllib2
+import requests
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
@@ -15,6 +15,7 @@ GPIO.setup(ECHO, GPIO.IN)
 GPIO.output(TRIG, False)
 
 print ("Waiting for sensor")
+time.sleep(2)
 
 GPIO.output(TRIG, True)
 time.sleep(0.00001)
@@ -31,12 +32,9 @@ pulse_duration = pulse_end - pulse_start
 distance = pulse_duration * 17150
 
 distance = round(distance, 2)
-
 print ("Distance:" , distance, " cm")
-
 url = 'http://waste-master.herokuapp.com/api/readings/'
-# payload = {'some': 'data'}
-payload = [{"container":1,"value":124}]
+payload = {"container":1,"value": 4120}
 headers = {'content-type': 'application/json'}
 
 response = requests.post(url, data=json.dumps(payload), headers=headers)
