7c90e7e9557b825461ac3202a7b62e1f1a1d3151
asjaddev@gmail.com <asjaddev@gmail.com>
2016-04-17 03:08:12 +0200

    ewms

--- EWMS/MQTT/mqtt_led_example.py
+++ EWMS/MQTT/mqtt_led_example.py
@@ -1,6 +1,6 @@
 import paho.mqtt.publish as publish
 import time
-print("Sending 0...")
-publish.single("debug", "0", hostname="10.0.0.129")
-print("Sending 1...")
-publish.single("debug", "1", hostname="10.0.0.129")
+print("Sender verdi 0...")
+publish.single("ewms", "0", hostname="10.0.0.129")
+print("Sender verdi 1...")
+publish.single("ewms", "1", hostname="10.0.0.129")
