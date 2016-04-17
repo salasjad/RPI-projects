66e2ac4c5785d0038d22f84ec8a369b059c122f9
asjaddev@gmail.com <asjaddev@gmail.com>
2016-04-18 01:33:14 +0200

    bla

--- EWMS/ultrasonic.py
+++ EWMS/ultrasonic.py
@@ -4,27 +4,36 @@ try:
     import time
     GPIO.setmode(GPIO.BCM)
     
-    TRIG = 23
-    ECHO = 24
-    
+    TOPIC = "ewms/container1"
+    HOSTNAME = "10.0.0.129"
+
+    TRIG1 = 23 #gul
+    ECHO1 = 24 #grønn
+    TRIG2 = 20 #oransje
+    ECHO2 = 21 #brun
     print ("Distance Measurement In Progress")
     
-    GPIO.setup(TRIG, GPIO.OUT)
-    GPIO.setup(ECHO, GPIO.IN)
+    GPIO.setup(TRIG1, GPIO.OUT)
+    GPIO.setup(TRIG2, GPIO.OUT)
+    GPIO.setup(ECHO1, GPIO.IN)
+    GPIO.setup(ECHO2, GPIO.IN)
 
-    #GPIO.output(TRIG, False)
-    print ("Waiting for sensor")
+    #GPIO.output(TRIG1, False)
+    #GPIO.output(TRIG2, Flase)
+    print ("Venter paa sensorene")
     #time.sleep(1)
     
     while True:
-        GPIO.output(TRIG, True)
+        GPIO.output(TRIG1, True)
+        GPIO.output(TRIG2, True)
         time.sleep(0.00001)
-        GPIO.output(TRIG, False)
+        GPIO.output(TRIG1, False)
+        GPIO.output(TRIG2, False)
         
-        while GPIO.input(ECHO)== 0:
+        while GPIO.input(ECHO1)== 0:
             pulse_start = time.time()
             
-        while GPIO.input(ECHO)==1:
+        while GPIO.input(ECHO1)==1:
             pulse_end = time.time()
             
         try:
@@ -35,9 +44,14 @@ try:
         distance = pulse_duration * 17150
         distance = round(distance, 2)
         print ("Distance:" , distance, " cm")
-        publish.single("ewms/container1", distance, hostname="10.0.0.129")
+        #publish.single("ewms/container1", distance, hostname="10.0.0.129")
+        publish_mqtt(distance)
         
         time.sleep(2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
+
+def publish_mqtt(message):
+    print("Distance:", message, " cm")
+    publish.single(TOPIC, message, HOSTNAME)
