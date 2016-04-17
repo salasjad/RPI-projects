7e4f6f4b72c730472831224d70db96f772e82fcc
Salahuddin Asjad <asjaddev@gmail.com>
2016-04-17 00:58:58 +0000

    added working mqtt communication

--- EWMS/MQTT/mqtt_led_example.py
+++ EWMS/MQTT/mqtt_led_example.py
@@ -1,7 +1,6 @@
 import paho.mqtt.publish as publish
 import time
 print("Sending 0...")
-publish.single("ledStatus", "0", hostname="macman")
-time.sleep(1)
+publish.single("debug", "0", hostname="10.0.0.129")
 print("Sending 1...")
-publish.single("ledStatus", "1", hostname="macman")
+publish.single("debug", "1", hostname="10.0.0.129")
