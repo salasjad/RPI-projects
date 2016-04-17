76c9759f5e6fee9c2b304f92872799f4aebb2a7a
asjaddev@gmail.com <asjaddev@gmail.com>
2016-04-18 01:58:03 +0200

    asfadklfs

--- EWMS/ultrasonic.py
+++ EWMS/ultrasonic.py
@@ -6,53 +6,52 @@ GPIO.setmode(GPIO.BCM)
 TOPIC = "ewms/container1"
 HOSTNAME = "10.0.0.129"
 
-def publish_mqtt(message):
-    print("Distance:", message, " cm")
-    publish.single(TOPIC, message, HOSTNAME)
-
-
 TRIG1 = 23 #gul
 ECHO1 = 24 #gronn
 TRIG2 = 20 #oransje
 ECHO2 = 21 #brun
-print ("Distance Measurement In Progress")
 
-try:   
-    GPIO.setup(TRIG1, GPIO.OUT)
-    GPIO.setup(TRIG2, GPIO.OUT)
-    GPIO.setup(ECHO1, GPIO.IN)
-    GPIO.setup(ECHO2, GPIO.IN)
+def publish_mqtt(message):
+    print("Distance:", message, " cm")
+    publish.single(TOPIC, message, HOSTNAME)
 
-    #GPIO.output(TRIG1, False)
-    #GPIO.output(TRIG2, Flase)
-    print ("Venter paa sensorene")
-    #time.sleep(1)
-    
-    while True:
-        GPIO.output(TRIG1, True)
-        GPIO.output(TRIG2, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG1, False)
-        GPIO.output(TRIG2, False)
+def ultrasonic_read:
+    try:   
+        GPIO.setup(TRIG1, GPIO.OUT)
+        GPIO.setup(TRIG2, GPIO.OUT)
+        GPIO.setup(ECHO1, GPIO.IN)
+        GPIO.setup(ECHO2, GPIO.IN)
+
+        #GPIO.output(TRIG1, False)
+        #GPIO.output(TRIG2, Flase)
+        print ("Venter paa sensorene")
+        #time.sleep(1)
         
-        while GPIO.input(ECHO1)== 0:
-            pulse_start = time.time()
+        while True:
+            GPIO.output(TRIG1, True)
+            GPIO.output(TRIG2, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG1, False)
+            GPIO.output(TRIG2, False)
             
-        while GPIO.input(ECHO1)==1:
-            pulse_end = time.time()
-            
-        try:
-		pulse_duration = pulse_end - pulse_start
-	except NameError:
-		pulse_duration = 0
+            while GPIO.input(ECHO1)== 0:
+                pulse_start = time.time()
+                
+            while GPIO.input(ECHO1)==1:
+                pulse_end = time.time()
+                
+            try:
+                    pulse_duration = pulse_end - pulse_start
+            except NameError:
+                    pulse_duration = 0
 
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
-        print ("Distance:" , distance, " cm")
-        #publish.single("ewms/container1", distance, hostname="10.0.0.129")
-        publish_mqtt(distance)
-        
-        time.sleep(2)
+            distance = pulse_duration * 17150
+            distance = round(distance, 2)
+            print ("Distance:" , distance, " cm")
+            #publish.single("ewms/container1", distance, hostname="10.0.0.129")
+            publish_mqtt(distance)
+            
+            time.sleep(2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
