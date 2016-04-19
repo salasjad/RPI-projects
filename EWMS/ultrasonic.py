17b1c0934acaacd6a91b8415de04dfd6bc69a0f7
asjaddev@gmail.com <asjaddev@gmail.com>
2016-04-19 21:00:44 +0200

    summing of average

--- EWMS/ultrasonic.py
+++ EWMS/ultrasonic.py
@@ -19,7 +19,7 @@ def send_update(message):
 
 def sensor_average(r_sensor):
     #average = reduce(lambda x, y: x + y, r_sensor)/len(r_sensor)
-    average = sum(r_sensor)/len(r_sensor)
+    average = sum(r_sensor)/len(r_sensor) if len(r_sensor) > 0 else 0
     return average
 
 try:   
@@ -47,19 +47,32 @@ try:
         GPIO.output(TRIG2, False)
         
         while GPIO.input(ECHO1)== 0:
-            pulse_start = time.time()
+            pulse_start1 = time.time()
             
         while GPIO.input(ECHO1)==1:
-            pulse_end = time.time()
-            
-        try:
-                pulse_duration = pulse_end - pulse_start
-        except NameError:
-                pulse_duration = 0
-
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
+            pulse_end1 = time.time()
         
+        while GPIO.input(ECHO2)== 0:
+            pulse_start2 = time.time()
+            
+        while GPIO.input(ECHO2)==1:
+            pulse_end2 = time.time()
+
+        try:
+                pulse_duration1 = pulse_end1 - pulse_start1
+                pulse_duration2 = pulse_end2 - pulse_start2
+        except NameError:
+                #pulse_duration1 = 0
+                #pulse_duration2 = 0
+
+        distance1 = pulse_duration1 * 17150
+        distance1 = round(distance1, 2)
+        
+        distance2 = pulse_duration2 * 17150
+        distance2 = round(distance2, 2)
+       
+        r_sensor1.append(distance1)
+        r_sensor2.append(distance2)
         average1 = sensor_average(r_sensor1)
         average2 = sensor_average(r_sensor2)
         average = max(average1, average2)
