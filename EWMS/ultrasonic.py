2dc0cb1c7a6544fdf7604b228b9745c01016feb9
asjaddev@gmail.com <asjaddev@gmail.com>
2016-04-19 22:08:10 +0200

    added second sensor

--- EWMS/ultrasonic.py
+++ EWMS/ultrasonic.py
@@ -18,20 +18,19 @@ def send_update(message):
     publish.single(TOPIC, message, hostname=HOSTNAME)
 
 def sensor_average(r_sensor):
-    #average = reduce(lambda x, y: x + y, r_sensor)/len(r_sensor)
     average = sum(r_sensor)/len(r_sensor) if len(r_sensor) > 0 else 0
     return average
 
 try:   
     GPIO.setup(TRIG1, GPIO.OUT)
-    #GPIO.setup(TRIG2, GPIO.OUT)
+    GPIO.setup(TRIG2, GPIO.OUT)
     GPIO.setup(ECHO1, GPIO.IN)
-    #GPIO.setup(ECHO2, GPIO.IN)
+    GPIO.setup(ECHO2, GPIO.IN)
 
-    #GPIO.output(TRIG1, False)
-    #GPIO.output(TRIG2, Flase)
+    GPIO.output(TRIG1, False)
+    GPIO.output(TRIG2, False)
     print ("Venter paa sensorene")
-    #time.sleep(1)
+    time.sleep(1)
    
 
     prev_average = 0
@@ -40,10 +39,10 @@ try:
 
     while True: 
         GPIO.output(TRIG1, True)
-        #GPIO.output(TRIG2, True)
+        GPIO.output(TRIG2, True)
         time.sleep(0.00001)
         GPIO.output(TRIG1, False)
-        #GPIO.output(TRIG2, False)
+        GPIO.output(TRIG2, False)
         
         while GPIO.input(ECHO1)== 0:
             pulse_start1 = time.time()
@@ -51,30 +50,33 @@ try:
         while GPIO.input(ECHO1)==1:
             pulse_end1 = time.time()
         
-        '''while GPIO.input(ECHO2)== 0:
+        while GPIO.input(ECHO2)== 0:
             pulse_start2 = time.time()
             
         while GPIO.input(ECHO2)==1:
             pulse_end2 = time.time()
-        '''
+        
         try:
                 pulse_duration1 = pulse_end1 - pulse_start1
-                #pulse_duration2 = pulse_end2 - pulse_start2
         except NameError:
                 pulse_duration1 = 0
-                #pulse_duration2 = 0
+
+        try:
+                pulse_duration2 = pulse_end2 - pulse_start2
+        except NameError:
+                pulse_duration2 = 0
 
         distance1 = pulse_duration1 * 17150
         distance1 = round(distance1, 2)
         
-        '''distance2 = pulse_duration2 * 17150
-        distance2 = round(distance2, 2) '''
+        distance2 = pulse_duration2 * 17150
+        distance2 = round(distance2, 2)
       
         r_sensor1.append(distance1)
-        #r_sensor2.append(distance2)
+        r_sensor2.append(distance2)
         average1 = sensor_average(r_sensor1)
-        #average2 = sensor_average(r_sensor2)
-        average2 = 0 #FIXME fjern senere
+        average2 = sensor_average(r_sensor2)
+        #average2 = 0 #FIXME fjern senere
         average = int(max(average1, average2))
 
 	print average
@@ -83,7 +85,7 @@ try:
             send_update(average)
             prev_average = average
         
-        time.sleep(0.5)
+        time.sleep(0.2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
